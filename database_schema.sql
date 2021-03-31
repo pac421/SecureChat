3771c76cd6a2c78460b3130f9e583cf1814a73ed
Paul-Alexandre CIESLIK <paul-alexandre.cieslik@hotmail.com>
2021-03-31 19:40:03 +0200

    first commit

---
+++ database_schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `USER`
+(
+    id VARCHAR(8) PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL
+);
